d391e3484ccb4b428f4aaf0392c657abca3e2a1f
dvir <dvir.asaf@live.biu.ac.il>
2020-10-20 22:19:10 +0300

    func : var, cov, pearson

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::endl;
+
+float bla1(float* x, int size) {
+    int i = 0;
+    float v = 0;
+    while (i != size) {
+        v = v + x[i];
+        i++;
+    }
+    return v / size;
+}
+
+float bla2(float* x, int size) {
+    int i = 0;
+    float v = 0;
+    float f = 0;
+    while (i != size) {
+        f = x[i] * x[i];
+        v = v + f;
+        i++;
+    }
+    return v / size;
+}
+
+float var(float* x, int size){
+    float z = bla2(x, size);
+    float w = bla1(x, size);
+    w = w * w;
+    return z - w;
+}
+
+// returns the covariance of X and Y
+float cov(float* x, float* y, int size){
+    float covXAvg = bla1(x, size);
+    float covYAvg = bla1(y, size);
+    int i = 0;
+    int j;
+    float k = 0;
+    while (i != size){
+    j = (x[i] - covXAvg) * (x[i] - covYAvg);
+    k = k + j;
+        i++;
+    }
+    return k / size;
+}
+
+// returns the Pearson correlation coefficient of X and Y
+float pearson(float* x, float* y, int size){
+    float avg = cov(x, y, size);
+    float rootX = bla1(x, size);
+    rootX = sqrtf(rootX);
+    float rootY = bla1(y, size);
+    rootY = sqrtf(rootY);
+    float root = rootX * rootY;
+    return avg / root;
+}
+
+int main() {
+    float x[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    float y[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    float avg = pearson(x, y, 10);
+    cout << avg << endl;
+    return 0;
+
+}
